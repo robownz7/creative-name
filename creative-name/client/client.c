6a957db21ea93b539838ab23315767ea5f4939a1
Rob Lowell <robob1988@gmail.com>
2016-03-08 21:27:27 -0500

    Creating client starter file

---
+++ creative-name/client/client.c
@@ -0,0 +1,15 @@
+// client.cpp : Defines the entry point for the console application.
+//
+
+#include "stdio.h"
+
+int main()
+{
+	printf("Hello World!\n");
+	printf("Hello World 2!\n");
+
+	getchar();
+
+    return 0;
+}
+
